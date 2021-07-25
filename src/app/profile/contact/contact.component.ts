c32a591c2924c5da5d2bd5868605f51e76d767fb
hamzanasri123 <hamzanasri16@gmail.com>
2021-07-26 00:42:27 +0100

    portfolio

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -11,23 +11,23 @@ import { environment } from '../../../environments/environment';
   styleUrls: ['./contact.component.scss']
 })
   export class ContactComponent implements OnInit {
- 
+
     model: any = {};
-  
+
     constructor(
       private http: HttpClient
     ){}
-  
+
     ngOnInit() {
        }
        onSubmit(name, subject, email, message) {
           const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-          this.http.post('https://formspree.io/f/mwkwpzve',
+          this.http.post('https://formspree.io/f/xoqynbgk',
             { name: name, subject: subject, replyto: email, message: message },
             { 'headers': headers }).subscribe(
               response => {
                 console.log(response);
               }
             );
-        }  
+        }
 }
